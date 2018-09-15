3bd023c9aabe58fd17dad7f40df3f9ae1d4f63a7
Sheelabhadra Dey <sheelabhadra@gmail.com>
2018-09-15 17:53:36 -0500

    Add check if tree is symmetric

---
+++ Trees/check_symmetric.py
@@ -0,0 +1,27 @@
+# Test if a binary tree is symmetric
+
+# Write a program that checks whether a binary tree is symmetric
+
+from TreeNode import Node
+
+def is_symmetric(root):
+	def check_subtree(root1, root2):
+		if not root1 and not root2:
+			return True
+		return root1.val == root2.val and check_subtree(root1.left, root2.right)\
+		 and check_subtree(root1.right, root2.left)
+
+	if not root:
+		return True
+	return check_subtree(root.left, root.right)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(2)
+root.left.left = Node(3)
+root.left.right = Node(4)
+root.right.left = Node(4)
+root.right.right = Node(3)
+
+print("Is the tree symmetric?	{}".format(is_symmetric(root)))
