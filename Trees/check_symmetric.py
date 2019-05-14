1221082a6e677e5fcb47a7c77a69ddfc8b7d157e
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-14 13:36:41 -0500

    Added correction to cover a few cases

--- Trees/check_symmetric.py
+++ Trees/check_symmetric.py
@@ -8,6 +8,10 @@ def is_symmetric(root):
 	def check_subtree(root1, root2):
 		if not root1 and not root2:
 			return True
+		if not root1 and root2:
+			return False
+		if root1 and not root2:
+			return False
 		return root1.val == root2.val and check_subtree(root1.left, root2.right)\
 		 and check_subtree(root1.right, root2.left)
 
